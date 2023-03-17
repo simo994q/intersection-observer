ee5f78397569dbb246629b2b5d4393d9319c4682
simo994q <simonkn1@hotmail.com>
2023-03-17 15:18:57 +0100

    afg

---
+++ assets/js.js
@@ -0,0 +1,18 @@
+
+const cards = document.querySelectorAll('.card')
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+        if (entry.isIntersecting) observer.unobserve(entry.target)
+    })
+},{
+    // threshold: 0.5,
+    rootMargin: '-50px',
+})
+
+
+cards.forEach(card => {
+    observer.observe(card)
+})
